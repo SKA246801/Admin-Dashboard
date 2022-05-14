bc8b0c6cf5c557bac6028e8fc8d61b4e7783d874
Sebastian Alvarado <sebastianalvarado246801@gmail.com>
2022-05-14 09:49:43 -0500

    pages created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
+import Home from './Pages/Home/Home'
+import Login from './Pages/Login/Login'
 
 function App() {
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          {/* <Navbar /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
