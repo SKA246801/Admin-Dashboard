f2e0640c657a0f7babd4fa314d8ca7cb9ac47c28
Sebastian Alvarado <sebastianalvarado246801@gmail.com>
2022-05-15 23:14:17 -0500

    admin can temporarily delete users

--- src/Components/DataTable/DataTable.js
+++ src/Components/DataTable/DataTable.js
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './DataTable.css'
 import { DataGrid } from '@mui/x-data-grid'
 import { columns, rows } from '../../Assets/Utils/TestData'
 import { Link } from 'react-router-dom'
 
 function DataTable() {
+  const [data, setData] = useState(rows)
+
+  const handleDelete = id => {
+    setData(data.filter(item => item.id !== id))
+  }
+
   const actionColumn = [
     {
       field: 'action',
       headerName: 'Action',
       width: 200,
-      renderCell: () => {
+      renderCell: params => {
         return (
           <div className='cellAction'>
             <Link to='/users/test' style={{ textDecoration: 'none' }}>
               <div className='viewButton'>View</div>
             </Link>
-            <div className='deleteButton'>Delete</div>
+            <div className='deleteButton' onClick={() => handleDelete(params.row.id)}>
+              Delete
+            </div>
           </div>
         )
       },
@@ -30,7 +38,7 @@ function DataTable() {
           Add New
         </Link>
       </div>
-      <DataGrid className='dataGrid' rows={rows} columns={columns.concat(actionColumn)} pageSize={10} rowsPerPageOptions={[10]} checkboxSelection />
+      <DataGrid className='dataGrid' rows={data} columns={columns.concat(actionColumn)} pageSize={10} rowsPerPageOptions={[10]} checkboxSelection />
     </div>
   )
 }
